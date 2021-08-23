41283e17e008f250a13a26c9a8974ae1bcbb9f2b
flying-酷酷的鹏sir <qingkunpeng@qq.com>
2021-08-23 17:34:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import re
 
 from github import Github
 
-MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions
+MD_HEAD = """**<p align="center">[Flying's Blog](https://blog.leeyom.top)</p>**
+**<p align="center">人生无非一场虚空大梦 韶华白首 不过转瞬</p>**
+## 联系方式
+- Email：[qingkunpeng@qq.com](mailto:qingkunpeng@qq.com)
+- Blog：[https://github.com/mylydg/gitblog](https://github.com/mylydg/gitblog)
 """
 
 BACKUP_DIR = "BACKUP"
